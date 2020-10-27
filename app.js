616df0ac190192501bd15bc3a888ae67ed815e2c
Akshit Kumar <akshitkumar450@gmail.com>
2020-10-28 01:22:38 +0530

    delete request done

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
+//tours is an array of all tours available
 const tours = JSON.parse(fs.readFileSync(__dirname + '/dev-data/data/tours-simple.json', 'utf-8'))
 
 app.get('/api/v1/tours', (req, res) => {
@@ -20,30 +20,33 @@ app.get('/api/v1/tours', (req, res) => {
 })
 
 app.get('/api/v1/tours/:id', (req, res) => {
-    const id=req.params.id*1 // to convert a string to num
-      const tour=tours.find(el=>el.id===id)
-      //to handle the error if the user has enter the id greater than tours array
-      
+    const id = req.params.id * 1 // to convert a string to num
+    const tour = tours.find(el => el.id === id)
+    //to handle the error if the user has enter the id greater than tours array
+
     // if(id>tours.length) {
-      if(!tour) {
-      return res.status(404).json({
-              status:'fail',
-             message:'invalid id'
-          })
-      }
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'invalid id'
+        })
+    }
+
     res.status(200).json({
         //JSend method
         status: 'success',
         data: {
-            tour:tour
+            tour: tour
         }
     })
 })
 
-
 app.post('/api/v1/tours', (req, res) => {
     console.log(req.body);
+    // to make id for new tour 
     const newid = tours[tours.length - 1].id + 1;
+
+    // create new tour
     // object.assign  make a new object by joining two existing object
     const newTour = Object.assign({ id: newid }, req.body)
     tours.push(newTour)
@@ -59,9 +62,21 @@ app.post('/api/v1/tours', (req, res) => {
     })
 })
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'invalid id'
+        })
+    }
+    res.status(204).json({
+        //JSend method
+        status: 'success',
+        data: null
+    })
 
-
-
+})
 app.listen(4000, () => {
     console.log('server started');
 })
