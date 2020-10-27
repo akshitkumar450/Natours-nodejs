37f43d1f4d835e2e52142845b8bc325993b043cc
Akshit Kumar <akshitkumar450@gmail.com>
2020-10-28 00:57:30 +0530

    getting tour by id

--- app.js
+++ app.js
@@ -19,6 +19,28 @@ app.get('/api/v1/tours', (req, res) => {
     })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id=req.params.id*1 // to convert a string to num
+      const tour=tours.find(el=>el.id===id)
+      //to handle the error if the user has enter the id greater than tours array
+      
+    // if(id>tours.length) {
+      if(!tour) {
+      return res.status(404).json({
+              status:'fail',
+             message:'invalid id'
+          })
+      }
+    res.status(200).json({
+        //JSend method
+        status: 'success',
+        data: {
+            tour:tour
+        }
+    })
+})
+
+
 app.post('/api/v1/tours', (req, res) => {
     console.log(req.body);
     const newid = tours[tours.length - 1].id + 1;
