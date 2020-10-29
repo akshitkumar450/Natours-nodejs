cb1ad183b588541754642d3a636204f83f313f2f
Akshit Kumar <akshitkumar450@gmail.com>
2020-10-29 23:47:14 +0530

    creating a schema and model

--- server.js
+++ server.js
@@ -16,6 +16,26 @@ mongoose.connect(DB, {
     console.log('db connection success');
 })
 
+// defining a schema for our tours
+const tourSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'a tour name is missing'],
+        unique:true
+    },
+    price:{
+        type:Number,
+        required:[true,'a tour price is missing']
+    },
+    rating:{
+        type:Number,
+        default:4.5
+    }
+})
+
+// creating a model for our DB
+const tour=mongoose.model('Tour',tourSchema)
+
 
 const port = process.env.PORT || 4000
 app.listen(port, () => {
