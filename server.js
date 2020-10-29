5e3b89c856ccaef74236db57bdc906bcc2dfcfc1
Akshit Kumar <akshitkumar450@gmail.com>
2020-10-30 00:08:40 +0530

    creating the tour

--- server.js
+++ server.js
@@ -8,34 +8,51 @@ const app = require('./app')
 
 //**MONGOOSE */
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+// it returns a promise
 mongoose.connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true 
 }).then(() => {
     console.log('db connection success');
 })
 
 // defining a schema for our tours
-const tourSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'a tour name is missing'],
-        unique:true
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'a tour name is missing'],
+        unique: true
     },
-    price:{
-        type:Number,
-        required:[true,'a tour price is missing']
+    price: {
+        type: Number,
+        // it is validator 
+        required: [true, 'a tour price is missing']
     },
-    rating:{
-        type:Number,
-        default:4.5
+    rating: {
+        type: Number,
+        default: 4.5
     }
 })
 
 // creating a model for our DB
-const tour=mongoose.model('Tour',tourSchema)
+const Tour = mongoose.model('Tour', tourSchema)
 
+// creating a new tour
+const testtour = new Tour({
+    name: 'camper park',
+    price: 697,
+  
+})
+
+// saving new tour in DB
+// it returns a promise
+testtour.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log('ERROR💥',err);
+})
 
 const port = process.env.PORT || 4000
 app.listen(port, () => {
