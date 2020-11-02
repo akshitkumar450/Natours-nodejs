86f94662a206cab00f55a8f22a94d39ef0e2db90
Akshit Kumar <akshitkumar450@gmail.com>
2020-11-03 01:25:42 +0530

    handling unhandled routes

--- app.js
+++ app.js
@@ -19,4 +19,14 @@ app.use('/', express.static(__dirname + '/public'))
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
+// handling the routes which are not declared by adding the middleware
+// if are able to run below code that means the req res cycle is not completes
+
+app.all('*', (req, res, next) => {
+    res.status(404).send({
+        status: 'fail',
+        message: `can't access the ${req.originalUrl} `
+    })
+})
+
 module.exports = app;
