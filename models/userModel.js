d57973d9b2c00f141d786a8da9300585335644ab
Akshit Kumar <akshitkumar450@gmail.com>
2020-11-08 12:01:25 +0530

    adding schema for user model

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, ' name should not be empty']
+    },
+    email: {
+        type: String,
+        required: [true, ' email should not be empty'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, ' email is not valid ']
+    },
+    photo: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: [true, ' password should not be empty'],
+        minlength: 8
+    },
+    confirmPassword: {
+        type: String,
+        required: [true, ' confirm your password'],
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
