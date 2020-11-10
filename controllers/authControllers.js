d676c61fc476506c5266f9e84d759fa7b60a3fba
Akshit Kumar <akshitkumar450@gmail.com>
2020-11-11 01:04:22 +0530

    sending JWT via cookie

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -16,6 +16,17 @@ function signToken(id) {
 
 function sendToken(user, statusCode, res) {
     const token = signToken(user._id)
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXIPIRES * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        cookieOptions.secure = true
+    }
+    res.cookie('jwt', token, cookieOptions)
+    // remove password from output
+    user.password = undefined
     res.status(statusCode).json({
         status: 'success',
         token: token,
