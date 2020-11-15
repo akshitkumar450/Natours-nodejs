727aff27245b8e276234240b353cadff16cf9723
Akshit Kumar <akshitkumar450@gmail.com>
2020-11-15 22:03:29 +0530

    added Outh in userRoutes file

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,31 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const {
-  getAllUsers,
-  getUserById,
-  createNewUser,
-  updateUser,
-  deleteUser,
-  updateMe, deleteMe, getMe
-} = require('../controllers/userController');
+const { getAllUsers, getUserById, createNewUser, updateUser, deleteUser, updateMe, deleteMe, getMe } =
+  require('../controllers/userController');
 
-const { signup, login, forgotPass, resetPass, updatePassword, protect } = require('./../controllers/authControllers')
+const { signup, login, forgotPass, resetPass, updatePassword, protect, restrictTo } = require('./../controllers/authControllers')
 
 router.post('/signup', signup)
 router.post('/login', login)
 router.post('/forgetPass', forgotPass)
 router.patch('/resetPass/:token', resetPass)
 
+//  ********* protect all routes after this middleware
+// router.use(protect)
+// we can define protect middleware at the starting from where we need to use protect middleware or we can put protect middleware in all the routes
+
 router.patch('/updatePass', protect, updatePassword)
 router.patch('/updateMe', protect, updateMe)
 router.delete('/deleteMe', protect, deleteMe)
 
-router.get('/', getAllUsers);
-router.get('/:id', getUserById);
-router.post('/', createNewUser);
-router.patch('/:id', updateUser);
-router.delete('/:id', deleteUser);
+//  we want all these to be used by whom...who is logged in and only to admins
+router.get('/', protect, restrictTo('admin'), getAllUsers);
+router.get('/:id', protect, restrictTo('admin'), getUserById);
+router.post('/', protect, restrictTo('admin'), createNewUser);
+router.patch('/:id', protect, restrictTo('admin'), updateUser);
+router.delete('/:id', protect, restrictTo('admin'), deleteUser);
 
-router.get('/me', protect, getMe, getUserById)
+router.get('/me', protect, getMe, getUserById), restrictTo('admin'),
 
-module.exports = router;
+  module.exports = router;
