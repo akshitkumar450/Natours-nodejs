6611e3ce02d23d96e67caf4e822e75c7af0bb8aa
Akshit Kumar <akshitkumar450@gmail.com>
2020-11-15 22:30:14 +0530

    importing user and review json file in DB

--- dev-data/data/import-dev.js
+++ dev-data/data/import-dev.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv')
 const fs = require('fs')
 dotenv.config({ path: './config.env' })
 const Tour = require('../../models/tourModel')
+const User = require('../../models/userModel')
+const Review = require('../../models/reviewModel')
 
 
 //**MONGOOSE */
@@ -21,12 +23,17 @@ mongoose.connect(DB, {
 
 // used for data modeling 
 const tours = JSON.parse(fs.readFileSync(__dirname + '/tours.json', 'utf-8'))
+const users = JSON.parse(fs.readFileSync(__dirname + '/users.json', 'utf-8'))
+const reviews = JSON.parse(fs.readFileSync(__dirname + '/reviews.json', 'utf-8'))
 
+// ************while imporing user  file just comment the password hashing fn  bcz in our json file we have stored the password after encryption only***
 
 //insert all data to DB from json file
 async function importData() {
     try {
         await Tour.create(tours)
+        await User.create(users, { validateBeforeSave: false }) // done this bcz when creating a user we need to be logged in so validateBefore will skip all vlaidators
+        await Review.create(reviews)
         console.log('data successfully loaded');
     } catch (err) {
         console.log(err);
@@ -38,6 +45,8 @@ async function importData() {
 async function deleteData() {
     try {
         await Tour.deleteMany()
+        await User.deleteMany()
+        await Review.deleteMany()
         console.log('data successfully deleted');
     } catch (err) {
         console.log(err);
