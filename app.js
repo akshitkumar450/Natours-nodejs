0c3f05a94afd051c2adc99e1a18d1d807f2f8ef5
akshit kumar <akshitkumar450@gmail.com>
2020-11-17 01:43:44 +0530

    extending base file'

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-const rateLimit = require('express-rate-limit')
-const helmet = require('helmet')
-const mongoSanitize = require('express-mongo-sanitize')
-const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const ApiErrors = require('./utils/apiErrors');
 const globalErrorHandler = require('./controllers/errorControllers');
 // used to log the request in terminal
 const morgan = require('morgan');
-const hpp = require('hpp')
-const path = require('path')
+const hpp = require('hpp');
+const path = require('path');
 
 const tourRouter = require('./routes/toursroutes');
 const userRouter = require('./routes/userroutes');
@@ -21,28 +21,24 @@ const reviewRouter = require('./routes/reviewroutes');
 // }
 
 // ******PUG template*****
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 // app.set('views',__dirname+'/views')
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 // security HTTP Headers HELMET MIDDLWARE ,,always on top of routes
-app.use(helmet())
-
+app.use(helmet());
 
 // Rate-limiter MiDDLEWARE
 const Limiter = rateLimit({
   // 100 requests from same IP  in 1 hour if exceed then error will come
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'too many requests from this IP ,please try again later'
-})
+  message: 'too many requests from this IP ,please try again later',
+});
 
-app.use('/api', Limiter)
-
+app.use('/api', Limiter);
 
 app.use(express.json({ limit: '10kb' }));
 /*  {
@@ -51,25 +47,25 @@ app.use(express.json({ limit: '10kb' }));
 }*/
 //  with above request we can login with the password without knowing email id ...bcz {"$gt":""} is always true
 
-//  DATA Sanitization against NOSQL query injection 
-app.use(mongoSanitize()) // this will fail the above code to login 
+//  DATA Sanitization against NOSQL query injection
+app.use(mongoSanitize()); // this will fail the above code to login
 
-//  DATA Sanitization against XSS 
-app.use(xss()) // it will prevent the from html code in request and convert them in  to spl. query 
+//  DATA Sanitization against XSS
+app.use(xss()); // it will prevent the from html code in request and convert them in  to spl. query
 // <div id='bad code'>mike </div>  ====>> ("&lt;div id='bad code'>mike &lt;/div>")
 
 // PREVENT POLLUTION PARAMETER
 // it will not allow duplicates in query string
-//  ?sort=duration&sort=price will not work 
-//  but after using hpp ?sort=dsuration&sort=price will only sort by price 
-app.use(hpp({
-  //  whitelist will allow the specific property to have duplicates in query string
-  whitelist: ['duration', 'ratingsAverage', 'price', 'difficulty']
-}))
-
+//  ?sort=duration&sort=price will not work
+//  but after using hpp ?sort=dsuration&sort=price will only sort by price
+app.use(
+  hpp({
+    //  whitelist will allow the specific property to have duplicates in query string
+    whitelist: ['duration', 'ratingsAverage', 'price', 'difficulty'],
+  })
+);
 
 // app.use('/', express.static(__dirname + '/public'));
-
 
 //** ROUTERS */
 
@@ -77,9 +73,21 @@ app.use(hpp({
 app.get('/', (req, res) => {
   res.status(200).render('base', {
     tour: 'the forest hiker',
-    user: 'nina '
-  })
-})
+    user: 'nina ',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'all tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'forest hiker',
+  });
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
