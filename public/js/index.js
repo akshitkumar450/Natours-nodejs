c73ad45514f83023e77054ae702f93b4e11c6296
Akshit Kumar <akshitkumar450@gmail.com>
2020-11-20 22:27:59 +0530

    updating user with our api

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,14 @@ console.log('bundle');
 import '@babel/polyfill'
 import { displayMap } from './mapbox'
 import { login, logout } from './login'
+import { updateData } from './update'
 
 //  DOM elements
 
 const mapBox = document.getElementById('map')
 const loginForm = document.querySelector('.form--login')
 const logoutBtn = document.querySelector('.nav__el--logout')
-
+const updateForm = document.querySelector('.form-user-data')
 
 
 if (mapBox) {
@@ -22,6 +23,14 @@ if (loginForm) {
         const email = document.getElementById('email').value
         const password = document.getElementById('password').value
         login(email, password)
+    })
+}
+if (updateForm) {
+    updateForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const email = document.getElementById('email').value
+        const name = document.getElementById('name').value
+        updateData(name, email)
     })
 }
 
---
+++ public/js/update.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import { showAlert } from './alerts'
+
+export const updateData = async (name, email) => {
+    try {
+        const res = await axios({
+            method: 'PATCH',
+            url: 'http://localhost:4000/api/v1/users/updateMe',
+            data: {
+                name: name,
+                email: email
+            }
+        })
+        if (res.data.status === 'success') {
+            showAlert('success', 'updated successfully')
+        }
+    }
+    catch (err) {
+        showAlert('error', err.response.data.message)
+    }
+}
